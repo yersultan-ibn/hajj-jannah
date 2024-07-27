e684d87f7e2beb2da59434b6ff96160a16961fe1
nurlykhan <nurlykhan.salamatuly@samgau.com>
2024-07-27 18:42:49 +0500

    feat: style step

--- src/modules/umra/UmraContent.tsx
+++ src/modules/umra/UmraContent.tsx
@@ -2,8 +2,34 @@ const root = {
   background: "url('./bg-1.webp')",
   height: '100vh',
   width: "100%",
+  overflow: "scroll"
 }
 
+const stepStyle = {
+  display: "inline-block",
+  border: "1px solid red",
+  width: 200,
+  padding: 15,
+  margin: 50,
+  background: "red",
+  color: "white",
+  fontSize: 20
+}
+
+const stepWrapperStyle = {
+}
+
+const steps = new Array(19).fill(0).map((_, i) => ({order: i+1}))
+
 export const UmraContent = () => {
-  return <div style={root}></div>;
+  console.log(steps)
+  return <div style={root}>
+    <div style={stepWrapperStyle}>
+    {steps.map(s => {
+      return <div style={stepStyle}>
+        {s.order}
+      </div>
+    })}
+    </div>
+  </div>;
 };
