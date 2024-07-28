87f9894199ed079f6e8082244f2b02465385b4c2
nurlykhan <nurlykhan.salamatuly@samgau.com>
2024-07-28 14:07:03 +0500

    feat: style edges

--- src/modules/umra/UmraContent.tsx
+++ src/modules/umra/UmraContent.tsx
@@ -1,3 +1,15 @@
+import React, {useEffect, useState} from 'react';
+import {
+  ReactFlow,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  MiniMap,
+  Controls,
+} from '@xyflow/react';
+
+import '@xyflow/react/dist/style.css';
+
 const root = {
   background: "url('./bg-1.webp')",
   height: '100vh',
@@ -16,20 +28,30 @@ const stepStyle = {
   fontSize: 20
 }
 
-const stepWrapperStyle = {
-}
+const initialNodes = [
+  { id: '1', position: { x: 25, y: 25 }, data: { label: '1' } },
+  { id: '2', position: { x: 200, y: 100 }, data: { label: '2' } },
+  { id: '3', position: { x: 25, y: 170 }, data: { label: '3' } },
+  { id: '4', position: { x: 200, y: 250 }, data: { label: '4' } },
+  { id: '5', position: { x: 100, y: 340 }, data: { label: '5' } },
 
-const steps = new Array(19).fill(0).map((_, i) => ({order: i+1}))
+];
+const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e2-3', source: '2', target: '3' },
+  { id: 'e3-5', source: '3', target: '4' },
+  { id: 'e4-5', source: '4', target: '5' },
+
+];
 
 export const UmraContent = () => {
-  console.log(steps)
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [bgColor, setBgColor] = useState();
+
   return <div style={root}>
-    <div style={stepWrapperStyle}>
-    {steps.map(s => {
-      return <div style={stepStyle}>
-        {s.order}
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <ReactFlow nodes={initialNodes} edges={initialEdges} />
       </div>
-    })}
-    </div>
   </div>;
 };
