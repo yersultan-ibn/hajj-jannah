eb0c941aaed59458779e5a9318ed0a3fbb4d2536
nurlykhan <nurlykhan.salamatuly@samgau.com>
2024-07-28 16:38:59 +0500

    feat: step info

--- src/modules/umra/CustomNode.tsx
+++ src/modules/umra/CustomNode.tsx
@@ -1,6 +1,22 @@
-import { Handle, Position, NodeToolbar } from "@xyflow/react";
+import { Handle, Position } from "@xyflow/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverFooter,
+  PopoverHeader,
+  PopoverTrigger,
+} from "@chakra-ui/react";
+import React from "react";
 
-const NodeStyle = {
+const NodeStyle: any = {
+  position: "relative",
+  textAlign: "center",
   backgroundColor: "#0000007d",
   backgroundRepeat: "no-repeat",
   color: "white",
@@ -11,7 +27,18 @@ const NodeStyle = {
   fontSize: 10,
 };
 
+const popoverTriggerStyle:any = {
+  position: "absolute",
+  top: 0,
+  height: "100%",
+  left: 0,
+  right: 0,
+  background: "red",
+  opacity: 0
+}
+
 type Props = {
+  id: string
   data: {
     label: string;
     width: number;
@@ -19,18 +46,16 @@ type Props = {
     bg: {
       url: string;
       size: number;
-      position?: string
+      position?: string;
     };
   };
 };
-export const CustomNode = ({ data }: Props) => {
-  const handleNodeClick = (e) => {
-    console.log(e);
-  };
+export const CustomNode = (props: Props) => {
+  const { data } = props
+  const initialFocusRef: any = React.useRef();
 
   return (
     <div
-      onClick={handleNodeClick}
       style={{
         ...NodeStyle,
         backgroundImage: `url(${data.bg.url})`,
@@ -38,16 +63,58 @@ export const CustomNode = ({ data }: Props) => {
         backgroundPosition: data.bg.position ? data.bg.position : "center 80%",
         width: data.width,
         height: data.height,
-        textAlign: "center",
       }}
     >
-      {/* <NodeToolbar isVisible>
+      <Popover
+        initialFocusRef={initialFocusRef}
+        placement="bottom"
+        closeOnBlur={false}
+      >
+        {/* <NodeToolbar isVisible>
         Toolbar
       </NodeToolbar> */}
-      {data.label}
+        {data.label}
+
       <Handle type="target" position={Position.Top} />
       <Handle type="source" position={Position.Bottom} id={data.label} />
       <Handle type="source" position={Position.Bottom} id={data.label} />
+
+        <PopoverTrigger>
+          <Button style={popoverTriggerStyle} />
+        </PopoverTrigger>
+
+        <PopoverContent
+          id="test"
+          color="white"
+          bg="blue.800"
+          borderColor="blue.800"
+        >
+          <PopoverHeader pt={4} fontWeight="bold" border="0">
+            {data.label}
+          </PopoverHeader>
+          <PopoverArrow bg="blue.800" />
+          <PopoverCloseButton />
+          <PopoverBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore.
+          </PopoverBody>
+          <PopoverFooter
+            border="0"
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            pb={4}
+          >
+            <Box fontSize="sm">Step {props.id} of 5</Box>
+            <ButtonGroup size="sm">
+              <Button colorScheme="green">Details</Button>
+              <Button colorScheme="blue" ref={initialFocusRef}>
+                Practice
+              </Button>
+            </ButtonGroup>
+          </PopoverFooter>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
--- src/modules/umra/StepDetails.tsx
+++ src/modules/umra/StepDetails.tsx
@@ -1,3 +1,4 @@
-export const StepDetails = () => {
 
+export const StepDetails = () => {
+return <></>
 }
